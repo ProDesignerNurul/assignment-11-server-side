7832758e096d41ac75be1fd949ee065c29902cd1
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-08 02:17:35 +0600

    adde some normal code

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+// server site testing 
+app.get('/', (req, res) => {
+    res.send('server is running now');
+});
+
+app.listen(port, () => {
+    console.log(`server is running now on port ${port}`);
+});
