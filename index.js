b6b3a3a7e94332e59b18485cb50e1fce2fbae997
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-09 02:18:43 +0600

    update some coded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,12 @@ async function run() {
     const cursor = serviceCollection.find(query);
     const services = await cursor.toArray();
     res.send(services);
+  })
+
+  app.get('/servicedetails/:id', (req, res) => {
+    const id = req.params.id;
+    const serviceDetails = allServices.find( service => service._d === ObjectId(id));
+    res.send(serviceDetails);
   })
 
   app.get('/services', async (req, res) => {
