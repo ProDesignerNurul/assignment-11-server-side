dff0de912d4be7b56e393a0fbefa9784758dabc1
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-08 16:08:31 +0600

    complate home service data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zwnyjff.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+  const serviceCollection = client.db('nurulRideShare').collection('rideServices');
+  const allServices = client.db('nurulRideShare').collection('allServices');
+
+  app.get('/allservices/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedService = allServices.find( service => service._id  == id);
+    res.send(selectedService);
+  })
+
+  app.get('/services', async (req, res) => {
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services);
+  })
+}
+run().catch(err => console.error(err))
 
 
 
